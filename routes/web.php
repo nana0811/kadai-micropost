3e3e054bef6d658bddb367c9a36375518275ebd3
EC2 Default User <ec2-user@ip-172-31-17-89.us-east-2.compute.internal>
2018-06-25 06:28:35 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,14 @@ Route::get('/', function () {
 
 Route::get('/', 'MicropostsController@index');
 
-Route::group(['middleware' => ['auth']], function(){
+Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
